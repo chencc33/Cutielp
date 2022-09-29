9d8e29e352657b13e80cb3047c625346fa598dda
chencc33 <chencc3333@gmail.com>
2022-09-29 00:55:15 -0700

    business feature working

--- react-app/src/components/Businesses/BusinessesList.js
+++ react-app/src/components/Businesses/BusinessesList.js
@@ -2,12 +2,16 @@ import businessesReducer, { getBusinesses } from "../../store/business";
 import { getBusinessesByUser } from "../../store/business";
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
+import BusinessForm from "./BusinessForm";
+import { useHistory } from "react-router-dom";
 
 const BusinessesList = () => {
     const dispatch = useDispatch()
-    console.log('***********in component*******')
+    const history = useHistory()
     const businesses = useSelector((state) => state.businesses)
     const businessesArr = Object.values(businesses)
+
+    const [showForm, setShowForm] = useState(false)
 
     useEffect(() => {
         dispatch(getBusinesses())
@@ -16,19 +20,27 @@ const BusinessesList = () => {
     if (!businessesArr.length) return null
     return (
         <>
+            <button onClick={() => (setShowForm(true))}>Create a Business</button>
             {businessesArr.map((business) => (
-                <div className="business-container">
-                    <div className="business-image"></div>
+                <div className="business-container"
+                    key={business.id}
+                    onClick={() => { history.push(`/businesses/${business.id}`) }}>
+                    <div className="business-image">
+                        {business['Images']?.map((image) => (
+                            <img src={image.url} alt='Business Image' height={200} width={200} />
+                        ))}
+                    </div>
                     <div className="business-into-container">
                         <div className="business-name">{business.name}</div>
                         <div className="business-rating">
-                            <div className="stars"></div>
-                            <div className="review-num">d</div>
+                            <div className="stars">Stars: {business.avgStar}</div>
+                            <div className="review-num">Reviews: {business.numReview}</div>
                         </div>
-                        <div className="open-close">{business.close}</div>
+                        <div className="open-close">Close: {business.close}</div>
                     </div>
                 </div>
             ))}
+            {showForm && (<BusinessForm />)}
         </>
     )
 }
