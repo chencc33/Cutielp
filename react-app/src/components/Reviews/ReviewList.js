4b3a0e9628941ea0abbb03a85d0f8c13f97d8d41
chencc33 <chencc3333@gmail.com>
2022-09-30 22:24:41 -0700

    edit review working

--- react-app/src/components/Reviews/ReviewList.js
+++ react-app/src/components/Reviews/ReviewList.js
@@ -1,13 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { getReviewsByBusinessId } from "../../store/review"
+import ReviewForm from "./ReviewForm"
 
 const ReviewList = () => {
     const dispatch = useDispatch()
     const businessId = useParams()
     const reviews = useSelector((state) => state.reviews)
     const reviewsArr = Object.values(reviews)
+
+    const [showForm, setShowForm] = useState(false)
+    const [onClickId, setOnClickId] = useState(null)
 
     useEffect(() => {
         dispatch(getReviewsByBusinessId(businessId))
@@ -20,9 +24,15 @@ const ReviewList = () => {
                 <>
                     <div>{review.review}</div>
                     <div>Stars{review.stars}</div>
-                    <button>Edit </button>
+                    <button
+                        onClick={() => {
+                            setShowForm(true)
+                            setOnClickId(review.id)
+                        }}
+                    >Edit </button>
                 </>
             ))}
+            {showForm && (<ReviewForm reviewId={onClickId} />)}
         </div>
     )
 }
