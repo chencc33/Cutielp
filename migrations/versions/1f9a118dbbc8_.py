110786047953c3f48cfd92c66943181453945495
chencc33 <chencc3333@gmail.com>
2022-10-28 01:32:01 -0700

    image opacity

--- migrations/versions/1f9a118dbbc8_.py
+++ migrations/versions/1f9a118dbbc8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 1255d3fbef93
+Revision ID: 1f9a118dbbc8
 Revises: 
-Create Date: 2022-10-28 00:38:03.168674
+Create Date: 2022-10-28 01:26:18.553937
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1255d3fbef93'
+revision = '1f9a118dbbc8'
 down_revision = None
 branch_labels = None
 depends_on = None
